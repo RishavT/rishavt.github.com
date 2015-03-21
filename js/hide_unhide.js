34a90c6f979415a7e240fd1d0a6e783cb2a77ecd
Rishav Thakker <thakker.rishav@gmail.com>
2015-03-22 00:56:27 +0530

    lol

---
+++ js/hide_unhide.js
@@ -0,0 +1,16 @@
+function display(action, id)
+{
+    console.log(id);
+    if (action == 'show')
+    {
+        document.getElementById(id).style.display = "block";
+        document.getElementById(id + "_heading").href= "javascript:display('hide', '"+id+"')";
+    }
+
+    if (action == 'hide')
+    {
+        document.getElementById(id).style.display = "none";
+        document.getElementById(id + "_heading").href= "javascript:display('show', '"+id+"')";
+    }
+}
+
---
+++ js/websitege.js
@@ -0,0 +1,7 @@
+window.setTimeout("Tick()", 1000);
+function Tick() {
+window.setTimeout("Tick()", 1000);
+var current = new Date().getTime();
+var start = 1426963766876
+console.log(current);
+}
