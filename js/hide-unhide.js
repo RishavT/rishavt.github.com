17b799670bfceb1f10d0eedd9b99273ee2b10cb5
Rishav Thakker <thakker.rishav@gmail.com>
2015-03-22 15:41:40 +0530

    lol

--- js/hide-unhide.js
+++ js/hide-unhide.js
@@ -3,14 +3,21 @@ function display(action, id)
     console.log(id);
     if (action == 'show')
     {
-        document.getElementById(id).style.display = "block";
-        document.getElementById(id + "-heading").href= "javascript:display('hide', '"+id+"')";
+        items = document.getElementsByClassName('collapse');
+        for (var i = 0; i < items.length; i ++)
+        {
+            items[i].style.display = 'none';
+            document.getElementById(items[i].id + '-heading').href = "javascript:display('show', '"+items[i].id+"')";
+        }
+        document.getElementById(id).style.display = 'block';
+        document.getElementById(id + '-heading').href = "javascript:display('hide', '"+id+"')";
+        
     }
 
     if (action == 'hide')
     {
-        document.getElementById(id).style.display = "none";
-        document.getElementById(id + "-heading").href= "javascript:display('show', '"+id+"')";
+        document.getElementById(id).style.display = 'none';
+        document.getElementById(id + '-heading').href = "javascript:display('show', '"+id+"')";
     }
 }
 
